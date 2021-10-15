a1aa4dfef5fcce67589f71b5267d98641b7ea4d5
mr_siddique <aamirrahman32@gmail.com>
2021-10-15 14:34:37 +0530

    bug fixed in index js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('clinet/build'));
+    app.use(express.static('client/build'));
 
     const path=require('path');
     app.get('*',(req,res)=>{
